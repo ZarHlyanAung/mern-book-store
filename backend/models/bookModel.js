26908fcc0885e9760b6f577af21be5ab7bb2c69d
Zar Hlyan Aung <zarhlyanaung.ai@gmail.com>
2024-02-19 20:21:12 +0630

    add : bookModel

---
+++ backend/models/bookModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: trues,
+  }
+);
+
+export const Book = mongoose.model('Book', bookSchema);
