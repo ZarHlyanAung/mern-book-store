a1ec73ad56d28c79d7ba9009411e6b9c29e60727
Zar Hlyan Aung <zarhlyanaung.ai@gmail.com>
2024-02-19 19:27:54 +0630

    Add : database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import express from 'express';
-import { PORT } from './config.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5555;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening to: ${PORT}`);
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(234).send('Welcome to MERN book store');
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB Connected...');
+    app.listen(PORT, () => {
+      console.log(`App is listening to: ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
