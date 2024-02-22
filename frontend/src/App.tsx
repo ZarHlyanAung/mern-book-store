63236ca46942674ce7b6fc4a139b66d00957f3db
Zar Hlyan Aung <zarhlyanaung.ai@gmail.com>
2024-02-22 22:28:41 +0630

    add : frontend pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import { useEffect } from 'react';
-import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBook';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
-  useEffect(() => {
-    axios.get('https:localhost:5555/books').then((response) => {
-      console.log(response);
-    });
-  }, []);
-  return <div className="bg-green-400 h-[100vh]">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="books/create" element={<CreateBook />} />
+      <Route path="books/details/:id" element={<ShowBook />} />
+      <Route path="books/edit/:id" element={<EditBook />} />
+      <Route path="books/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
 };
 
 export default App;
