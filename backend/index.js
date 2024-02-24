caabcb30afbc8ff2454c3d33f71de45e7e3535f2
Zar Hlyan Aung <zarhlyanaung.ai@gmail.com>
2024-02-24 18:45:09 +0630

    add: Book Detail page

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // middleware for handing cors policy
 app.use(
   cors({
-    origin: 'http://localhost:5174',
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type'],
   })
